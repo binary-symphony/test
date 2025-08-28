9f37f6d3d673b7726dd1f3913d3e666c032a87dc
lazarus <williamcswll.thepequod@gmail.com>
2025-08-28 15:34:07 -0400

    first save, cage bonus

--- src/main.cpp
+++ src/main.cpp
@@ -54,11 +54,17 @@ void opcontrol() {
 	pros::Motor tR (-3, pros::MotorGears::red);
 	pros::Motor bL (2, pros::MotorGears::red);
 	pros::Motor bR (4, pros::MotorGears::red);
+	pros::Motor cage (5, pros::MotorGears::red);
 
 	while (true) {
 		int left = con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 		int right = con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-
+		if (con.get_digital(DIGITAL_R1)) {
+      		cage.move_velocity(100); // This is 100 because it's a 100rpm motor
+    	}
+		if (con.get_digital(DIGITAL_R2)) {
+	  		cage.move_velocity(-100); // This is -100 because it's a 100rpm motor
+		}
 		tL.move(left);
 		bL.move(left);
 
